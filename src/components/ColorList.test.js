a9a09045daa4fb77fbd6d8c4808c9f86c8a2727a
lexdechris23 <70167097+lexdechris23@users.noreply.github.com>
2021-08-15 20:16:16 -0500

    Finished building tests

--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
-
-import { render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ColorList from './ColorList';
 
+const colorTestList = [
+    {
+        color: 'aliceblue',
+        code: {hex: '#f0f8ff'},
+        id: 1,
+    },
+    {
+        color: 'limegreen',
+        code: {hex: '#99ddbc'},
+        id: 2,
+    },
+    {
+        color: 'aqua',
+        code: {hex: '#00ffff'},
+        id: 3,
+    },
+    {
+        color: "aquamarine",
+        code: {hex: "#7fffd4"},
+        id:  4,
+    }
+];
+
 test("Renders an empty list of colors without errors", () => {
+
+    render(<ColorList colors={[]} />);
+    
 });
 
 test("Renders a list of colors without errors", () => {
+
+    render(<ColorList colors={colorTestList} />);
+    const colorList = screen.getAllByTestId('color');
+    expect(colorList).toHaveLength(4);
+
 });
 
-test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+test("Renders the EditForm when editing = true and does not render EditForm when editing = false", async () => {
+    const { rerender } = render(<ColorList colors={colorTestList} editing={true} />);
+    await (() => {
+        const color = screen.getByText(/aqua/i);
+        userEvent.click(color);
+        const editColor = screen.queryAllByTestId('editMenu');
+        expect(editColor).not.toBeNull;
+        expect(editColor).toHaveLength(1);
+        rerender(<ColorList colors={colorTestList} editing={false} />);
+        const checkEditMenu = screen.queryAllByTestId('editMenu');
+        expect(checkEditMenu).toHaveLength(0);
+    })
 });
